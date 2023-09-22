f757eecdb2189ad61f0a3a17f8785706ff7604c7
AdenilsonFFerreira <adenilson.fernandes@aluno.ifsp.edu.br>
2023-09-22 09:57:45 -0300

    marques

--- MARQUES/ProjAções/WindowsFormsApp1/Form5.cs
+++ MARQUES/ProjAções/WindowsFormsApp1/Form5.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +17,103 @@ namespace WindowsFormsApp1
         public Form5()
         {
             InitializeComponent();
+        }
+        private void UpdateListView()
+        {
+            Conexao conn = new Conexao();
+            SqlCommand sqlCom = new SqlCommand();
+
+            sqlCom.Connection = conn.ReturnConnection();
+            sqlCom.CommandText = "SELECT * FROM PAPEL";
+
+            try
+            {
+                SqlDataReader dr = sqlCom.ExecuteReader();
+
+                //Enquanto for possível continuar a leitura das linhas que foram retornadas na consulta, execute.
+                while (dr.Read())
+                {
+                    string acao = (string)dr["Acao"];
+                    int qtd = (int)dr["Quantidade"];
+                    decimal valor = (decimal)(dr["Valor"]);
+
+                    ListViewItem lv = new ListViewItem();
+                    lv.SubItems.Add(acao);
+                    lv.SubItems.Add(qtd.ToString());
+                    lv.SubItems.Add(valor.ToString());
+                    listView1.Items.Add(lv);
+
+                }
+                dr.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            Conexao conexao = new Conexao();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = conexao.ReturnConnection();
+            sqlCommand.CommandText = @"INSERT INTO PAPEL VALUES (@acao, @quantidade, @valor)"
+            ;
+
+            sqlCommand.Parameters.AddWithValue("@acao", txbAcao.Text);
+            sqlCommand.Parameters.AddWithValue("@quantidade", txbQtd.Text);
+            sqlCommand.Parameters.AddWithValue("valor", txbValor.Text);
+            
+            try
+            {
+                //Insere o cliente
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao inserir colaborador no banco.\n"
+                    + err.Message);
+            }
+            finally
+            {
+                conexao.CloseConnection();
+            }
+            MessageBox.Show(
+                "Cadastrado com Sucesso",
+                "CADASTRO",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+            {
+                txbAcao.Clear();
+                txbQtd.Clear();
+                txbValor.Clear();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            UpdateListView();
         }
     }
 }
