309b98929c9b44ca361cc4a9028f16ca77816e51
Adenilson Fernandes Ferreira <adenilson.ff@outlook.com>
2024-06-14 20:25:36 -0300

    web-cadastro-ok

---
+++ SYS_CARDE/# CARDDE/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('veiculo').addEventListener('click', function() {
+        window.location.href = 'C:/GIT/SYS_CARDE/# CARDDE/cadastro_veiculo.html'; 
+    });
+
+    document.getElementById('estoque').addEventListener('click', function() {
+        window.location.href = 'cadastro_estoque.html'; 
+    });
+});
