749b7c8c69b6532724eadd9c5fc43115bbb06e75
Adenilson Fernandes Ferreira <adenilson.ff@outlook.com>
2024-06-17 20:10:35 -0300

    web-sair

--- SYS_CARDE/# CARDDE/js/sair.js
+++ SYS_CARDE/# CARDDE/js/sair.js
@@ -1,16 +1,15 @@
 
 // JavaScript para mostrar e esconder a caixa de diálogo
-document.querySelector('#btnSair button').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('msgSair').style.display = 'block';
-  });
-  
-  document.getElementById('confirmarSaida').addEventListener('click', function() {
-    // Código para sair da aplicação ou navegar para login.html
-    window.location.href = 'login.html';
-  });
-  
-  document.getElementById('cancelarSaida').addEventListener('click', function() {
-    document.getElementById('msgSair').style.display = 'none';
-  });
-  
+document.getElementById('btnSair').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('msgSair').style.display = 'block';
+});
+
+document.getElementById('confirmarSaida').addEventListener('click', function() {
+  // Código para sair da aplicação ou navegar para login.html
+  window.location.href = 'login.html';
+});
+
+document.getElementById('cancelarSaida').addEventListener('click', function() {
+  document.getElementById('msgSair').style.display = 'none';
+});
