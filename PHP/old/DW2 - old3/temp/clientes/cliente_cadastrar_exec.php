964667230dc2355861f45d0b1ac24a17c004ceac
Adenilson FF <adenilson.ff@outlook.com>
2024-10-04 11:14:38 -0300

    web acertado

---
+++ PHP/old/DW2 - old3/temp/clientes/cliente_cadastrar_exec.php
@@ -0,0 +1,46 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include('../../central/conexao.php');
+
+$codigo        = $_POST['codigo'];
+$nome_completo = $_POST['nome_completo'];
+$sexo          = $_POST['sexo'];
+$dt_nasc       = $_POST['dt_nasc'];
+$email         = $_POST['email'];
+$telefone      = $_POST['telefone'];
+
+/*
+echo "<br>Código do Cliente: $codigo<br>";
+echo "<br>Nome Completo: $nome_completo<br>";
+echo "<br>Dt. Nasc.: $dt_nasc<br>";
+echo "<br>Email: $email<br>";
+echo "<br>Telefone (zap): $telefone<br>";
+*/
+
+
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Receber os dados do formulário
+    $codigo = $_POST['codigo'];
+    $nome_completo = $_POST['nome_completo'];
+    $dt_nasc = $_POST['dt_nasc'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    // Criar a consulta SQL para inserir os dados na tabela clientes
+    $sql = "INSERT INTO clientes 
+    ( codigo,    nome_completo,    sexo,  dt_nasc,    email, telefone) VALUES 
+    ('$codigo', '$nome_completo', '$sexo', '$dt_nasc', '$email', '$telefone')";
+
+    // Executar a consulta e verificar se foi bem-sucedida
+    if (mysqli_query($conexao, $sql)) {
+        //echo "Cadastro realizado com sucesso!";
+        header("Location: clientes_listar.php");
+    } else {
+        echo "Erro: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+mysqli_close($conexao);
+?>
